81fc6c5e6589882f213f97bf44f2620b08f35f1e
David Sanders <dsanders@fusionbox.com>
2013-01-11 11:56:16 -0700

    Convert to using setuptools
    - Convert README to rst for pypi longdescription
    - Modify setup.py to import README

---
+++ README.rst
@@ -0,0 +1,61 @@
+====================
+django-ordered-model
+====================
+
+Allows models to be ordered and provides a simple admin interface for
+reordering them.
+
+Based on http://www.djangosnippets.org/snippets/998/ and
+http://www.djangosnippets.org/snippets/259/
+
+Requires:
+
+* Django 1.4
+
+Installation
+============
+
+::
+
+    $ python setup.py install
+
+Usage
+=====
+
+Add ``ordered_model`` to your ``SETTINGS.INSTALLED_APPS``.
+
+To add ordering to your model, inherit from the ``OrderedModel`` class::
+
+    from django.db import models
+
+    from ordered_model.models import OrderedModel
+
+    class Item(OrderedModel):
+        name = models.CharField(max_length=100)
+
+Model instances now have ``move_up()`` and ``move_down()`` methods to move them
+relative to each other.
+
+To add re-ordering controls to a model's admin interface, inherit from the
+``OrderedModelAdmin`` class.  Your model admin will now have a method called
+``order_links``, which outputs html for two buttons which control model
+ordering.  Use the admin class's ``list_display`` property to add these buttons
+into the admin interface::
+
+    from django.contrib import admin
+
+    from ordered_model.admin import OrderedModelAdmin
+
+    from .models import Item
+
+    class ItemAdmin(OrderedModelAdmin):
+        list_display = ('name', 'order_links')
+
+    admin.site.register(Item, ItemAdmin)
+
+Test suite
+==========
+
+::
+
+    $ ./run_tests.sh
--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from distutils.core import setup
- 
-setup(
-    name='django-ordered-model',
-    version='0.2.0',
-    description='Allows Django models to be ordered and provides a simple admin interface for reordering them.',
-    author='Ben Firshman',
-    author_email='ben@firshman.co.uk',
-    url='http://github.com/bfirsh/django-ordered-model/',
-    packages=[
-        'ordered_model',
-        'ordered_model.tests',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    package_data={'ordered_model': ['static/ordered_model/arrow-up.gif',
-                                    'static/ordered_model/arrow-down.gif',
-                                    'locale/de/LC_MESSAGES/django.po',
-                                    'locale/de/LC_MESSAGES/django.mo',
-                                    'locale/pl/LC_MESSAGES/django.po',
-                                    'locale/pl/LC_MESSAGES/django.mo']}
-)
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='django-ordered-model',
+      version='0.2.0',
+      description='Allows Django models to be ordered and provides a simple admin interface for reordering them.',
+      long_description=readme(),
+      author='Ben Firshman',
+      author_email='ben@firshman.co.uk',
+      url='https://github.com/bfirsh/django-ordered-model',
+      packages=[
+          'ordered_model',
+          'ordered_model.tests',
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+      package_data={
+          'ordered_model': [
+              'static/ordered_model/arrow-up.gif',
+              'static/ordered_model/arrow-down.gif',
+              'locale/de/LC_MESSAGES/django.po',
+              'locale/de/LC_MESSAGES/django.mo',
+              'locale/pl/LC_MESSAGES/django.po',
+              'locale/pl/LC_MESSAGES/django.mo',
+          ]
+      })
