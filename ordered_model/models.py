46eeadb38939789cec475b789a6e933323afb1b2
David Sanders <dsanders@fusionbox.com>
2013-01-11 11:47:52 -0700

    De-lint ordered_model/models.py
    - Remove unused imports
    - Update OrderedModel docstring
    - Minor commenting
    - PEP8 whitespace

--- ordered_model/models.py
+++ ordered_model/models.py
@@ -1,20 +1,20 @@
-from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
 from django.db import models
+
 
 class OrderedModel(models.Model):
     """
     An abstract model that allows objects to be ordered relative to each other.
-    Provides an ``order`` field.
+    Provides an ``order`` field and two methods -- ``move_up`` and
+    ``move_down``.
     """
-    
     order = models.PositiveIntegerField(editable=False)
-    
+
     class Meta:
         abstract = True
         ordering = ('order',)
-    
+
     def save(self, *args, **kwargs):
+        # place new items at end of list
         if not self.id:
             qs = self.__class__.objects.order_by('-order')
             try:
@@ -22,7 +22,7 @@ class OrderedModel(models.Model):
             except IndexError:
                 self.order = 0
         super(OrderedModel, self).save(*args, **kwargs)
-    
+
     def _move(self, up):
         qs = self.__class__._default_manager
         if up:
@@ -37,13 +37,13 @@ class OrderedModel(models.Model):
         self.order, replacement.order = replacement.order, self.order
         self.save()
         replacement.save()
-    
+
     def move_down(self):
         """
         Move this object down one position.
         """
         return self._move(up=False)
-    
+
     def move_up(self):
         """
         Move this object up one position.
